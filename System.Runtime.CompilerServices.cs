05e47344cec5013160751747823e177205212dd4
任天天 <ruattqwq@outlook.com>
2025-06-10 17:29:32 +0800

    feat: 基于访问路径路由的 HTTP 服务端

---
+++ System.Runtime.CompilerServices.cs
@@ -0,0 +1,3 @@
+﻿// ReSharper disable CheckNamespace, UnusedType.Global
+namespace System.Runtime.CompilerServices;
+internal static class IsExternalInit;
---
+++ Utils/StringStream.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PCL.Core.Utils;
+
+public class StringStream : Stream
+{
+    private readonly MemoryStream _innerStream;
+
+    /// <summary>
+    /// 使用默认的 UTF-8 编码初始化 StringStream。
+    /// </summary>
+    public StringStream(string source) : this(source, Encoding.UTF8) { }
+
+    /// <summary>
+    /// 使用指定编码初始化 StringStream。
+    /// </summary>
+    public StringStream(string source, Encoding encoding)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (encoding == null) throw new ArgumentNullException(nameof(encoding));
+
+        var buffer = encoding.GetBytes(source);
+        _innerStream = new MemoryStream(buffer);
+    }
+
+    public override bool CanRead  => _innerStream.CanRead;
+    public override bool CanSeek  => _innerStream.CanSeek;
+    public override bool CanWrite => false;
+    public override long Length   => _innerStream.Length;
+
+    public override long Position
+    {
+        get => _innerStream.Position;
+        set => _innerStream.Position = value;
+    }
+
+    public override void Flush() { /* 只读流 无需实现 */ }
+
+    public override int Read(byte[] buffer, int offset, int count) => _innerStream.Read(buffer, offset, count);
+
+    public override long Seek(long offset, SeekOrigin origin) => _innerStream.Seek(offset, origin);
+
+    public override void SetLength(long value) => throw new NotSupportedException("StringStream 是只读流，不支持 SetLength。");
+
+    public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException("StringStream 是只读流，不支持 Write。");
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing) _innerStream.Dispose();
+        base.Dispose(disposing);
+    }
+}
