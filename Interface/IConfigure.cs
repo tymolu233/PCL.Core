eac72d847b806283aef00c1135fb211c50205fef
tangge233 <tangge233@qq.com>
2025-06-14 23:08:05 +0800

    style: remove unused imports

--- Interface/IConfigure.cs
+++ Interface/IConfigure.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PCL.Core.Interface
+﻿namespace PCL.Core.Interface
 {
     public interface IConfigure
     {
