74686f26e18033b7c18de12affad6ac0557bc1dc
tangge233 <tangge233@qq.com>
2025-06-23 20:32:43 +0800

    chore(byte): 加入一个小工具

---
+++ Helper/ByteHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace PCL.Core.Helper;
+
+public class ByteHelper(Stream stream)
+{
+    private Stream _byteStream = stream;
+    
+    public long Length => _byteStream.Length;
+
+    public string GetReadableLength() => GetReadableLength(this.Length);
+    
+    public static string GetReadableLength(long length)
+    {
+        string[] unit = ["B", "KB", "MB", "GB", "TB", "PB"];
+        long displayCount = length * 100;
+        int displayUnit = 0;
+        while (displayCount >= 102400)
+        {
+            displayCount >>= 10;
+            displayUnit++;
+        }
+
+        if (displayUnit > unit.Length)
+            throw new IndexOutOfRangeException("Why there is no enough unit to show :(");
+        var displayText = displayCount.ToString();
+        displayText = displayText.Insert(displayText.Length - 2, ".");
+        return $"{displayText} {unit[displayUnit]}";
+    }
+}
