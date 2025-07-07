a77edd546bd5d3ea17f38d454c2e84d35ad7cc7a
任天天 <ruattqwq@outlook.com>
2025-07-07 10:49:20 +0800

    ref(Network): 使用公共属性，改进线程同步

--- Service/HttpClientService.cs
+++ Service/HttpClientService.cs
@@ -3,29 +3,31 @@ using System.Net.Http;
 using System.Threading;
 using PCL.Core.Helper;
 using PCL.Core.LifecycleManagement;
-using PCL.Core.Utils.Net;
+using PCL.Core.Utils;
 
 namespace PCL.Core.Service;
 
-[LifecycleService(LifecycleState.Loading, Priority = int.MaxValue)]
+[LifecycleService(LifecycleState.Loading)]
 public class HttpClientService : ILifecycleService
 {
     public string Identifier => "network";
     public string Name => "网络服务";
     public bool SupportAsyncStart => true;
-    
-    private readonly LifecycleContext Context;
-    private HttpClientService() { Context = Lifecycle.GetContext(this); }
 
-    private static readonly HttpProxyManager ProxyManager = new();
+    private static LifecycleContext? _context;
+    private static LifecycleContext Context => _context!;
+    private HttpClientService() { _context = Lifecycle.GetContext(this); }
+
+    public static HttpProxyManager Proxy { get; } = new();
+    
     private static HttpClientHandler _currentHandler = new()
     {
         UseProxy = true,
-        Proxy = ProxyManager
+        Proxy = Proxy
     };
+
     private static HttpClient _currentClient = new(_currentHandler);
-    private static HttpClientHandler? _previousHandler;
-    private static HttpClient? _previousClient;
+
     private static readonly object OperationLock = new();
 
     public static HttpClient GetClient()
@@ -36,20 +38,40 @@ public class HttpClientService : ILifecycleService
         }
     }
 
+    private static HttpClientHandler? _previousHandler;
+    private static HttpClient? _previousClient;
+
+    private static void _RefreshClient()
+    {
+        Context.Debug("正在刷新 HttpClient"); 
+        _previousClient?.Dispose(); 
+        _previousHandler?.Dispose(); 
+        _previousClient = _currentClient; 
+        _previousHandler = _currentHandler;
+        _currentHandler = new HttpClientHandler
+        {
+            UseProxy = true,
+            Proxy = Proxy
+        };
+        _currentClient = new HttpClient(_currentHandler); 
+        Context.Trace("已刷新 HttpClient");
+    }
+
+    private static readonly AutoResetEvent RefreshEvent = new(false);
+    private static readonly AutoResetEvent RefreshFinishedEvent = new(false);
+
     public static void RefreshClient()
     {
         lock (OperationLock)
         {
-            _previousClient?.Dispose();
-            _previousHandler?.Dispose();
-            _previousClient = _currentClient;
-            _previousHandler = _currentHandler;
-            _currentHandler = new HttpClientHandler();
-            _currentClient = new HttpClient(_currentHandler);
+            _manualRefresh = true;
+            RefreshEvent.Set();
+            RefreshFinishedEvent.WaitOne();
         }
     }
-    
-    private static readonly ManualResetEventSlim StopEvent = new(false);
+
+    private static bool _stopped = false;
+    private static bool _manualRefresh = false;
 
     public void Start()
     {
@@ -57,17 +79,24 @@ public class HttpClientService : ILifecycleService
         NativeInterop.RunInNewThread(() => {
             while (true)
             {
-                if (StopEvent.Wait(TimeSpan.FromHours(4))) break;
-                RefreshClient();
+                RefreshEvent.WaitOne(TimeSpan.FromHours(4));
+                if (_stopped) break;
+                _RefreshClient();
+                if (_manualRefresh)
+                {
+                    _manualRefresh = false;
+                    RefreshFinishedEvent.Set();
+                }
             }
-        });
+        }, "HttpClient Refresh");
     }
 
     public void Stop()
     {
         lock (OperationLock)
         {
-            StopEvent.Set();
+            _stopped = true;
+            RefreshEvent.Set();
             _currentClient.Dispose();
             _currentHandler.Dispose();
             _previousClient?.Dispose();
---
+++ Utils/HttpProxyManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+
+namespace PCL.Core.Utils;
+
+public class HttpProxyManager : IWebProxy
+{
+    private readonly object _operationLock = new();
+    private ICredentials? _credentials;
+
+    private string? _proxyAddress;
+
+    public ICredentials? Credentials
+    {
+        get
+        {
+            lock (_operationLock)
+            {
+                return _credentials;
+            }
+        }
+        set
+        {
+            lock (_operationLock)
+            {
+                _credentials = value ??
+                               throw new ArgumentNullException(nameof(Credentials), $"{nameof(Credentials)} 不能为 Null");
+            }
+        }
+    }
+
+    public string? ProxyAddress
+    {
+        get
+        {
+            lock (_operationLock)
+            {
+                return _proxyAddress;
+            }
+        }
+        set
+        {
+            lock (_operationLock)
+            {
+                _proxyAddress = value ?? "";
+            }
+        }
+    }
+
+    private WebProxy? _proxy;
+
+    private WebProxy? CurrentProxy
+    {
+        get
+        {
+            lock (_operationLock)
+            {
+                return _proxy;
+            }
+        }
+        set
+        {
+            lock (_operationLock)
+            {
+                _proxy = value;
+            }
+        }
+    }
+
+    public bool DisableProxy { get; set; }
+
+    private readonly WebProxy _systemProxy = new()
+    {
+        BypassProxyOnLocal = true
+    };
+
+    public bool RequireRefresh;
+
+    public Uri? GetProxy(Uri? uri)
+    {
+        return GetWebProxy(uri)?.Address ?? uri;
+    }
+
+    public WebProxy? GetWebProxy(Uri? uri)
+    {
+        if (CurrentProxy is not null && !RequireRefresh) return CurrentProxy;
+        RequireRefresh = false;
+        if (!string.IsNullOrWhiteSpace(_proxyAddress))
+        {
+            CurrentProxy = new WebProxy(_proxyAddress, true);
+            return CurrentProxy;
+        }
+
+        CurrentProxy = _systemProxy;
+        return CurrentProxy;
+    }
+
+    public bool IsBypassed(Uri? uri)
+    {
+        if (DisableProxy) return true;
+        return CurrentProxy?.IsBypassed(uri ?? new Uri("http://127.0.0.1")) ?? true;
+    }
+}
