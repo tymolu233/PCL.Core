8a2cda3d82147783d63806f203e61374c2e77dba
任天天 <ruattqwq@outlook.com>
2025-07-14 17:03:04 +0800

    feat(download): 完成文件分块模型和下载实现

---
+++ Utils/Download/DownloadItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PCL.Core.Utils.Download;
+
+public class DownloadItem(Uri uri, string targetPath, int chunkSize = 4096)
+{
+    public string TargetPath { get; } = Path.GetFullPath(targetPath);
+    
+    public Uri SourceUrl { get; set; } = uri;
+    
+    public long ContentLength { get; set; } = 0;
+    
+    public int ChunkSize { get; } = chunkSize;
+
+    public LinkedList<ParallelTask> Parallels { get; } = [];
+
+    public event Action? Finished;
+    
+    private int _finishedCount = 0;
+    
+    
+}
---
+++ Utils/Download/Downloader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+namespace PCL.Core.Utils.Download;
+
+public class Downloader(int maxParallels = 16)
+{
+    /// <summary>
+    /// 限制最大并发数量，该数量由所有实例共享。
+    /// </summary>
+    public static int ParallelTaskLimit { get; set; } = 16;
+    
+    /// <summary>
+    /// 限制最大并发数量，该数量仅作用于该实例。
+    /// </summary>
+    public int MaxParallels { get; set; } = maxParallels;
+    
+    private ConcurrentQueue<DownloadItem> _downloadQueue = [];
+}
