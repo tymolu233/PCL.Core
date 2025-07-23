350ba435c7189499b407ad129e3bd2c3ea40c337
Pigeon0v0 <60414767+Pigeon0v0@users.noreply.github.com>
2025-07-23 15:35:54 +0800

    feat(CmdLine): 添加了通过 PID 获取进程命令行参数的工具

---
+++ Helper/CmdLineHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Management;
+
+namespace PCL.Core.Helper
+{
+    public static class CmdLineHelper
+    {
+        public static string GetCommandLine(int pid)
+        {
+            string query = $"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {pid}";
+            using (var searcher = new ManagementObjectSearcher(query))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                    return obj["CommandLine"]?.ToString();
+            }
+            return null;
+        }
+    }
+}
