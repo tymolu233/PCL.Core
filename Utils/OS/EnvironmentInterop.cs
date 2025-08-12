f9a4fa0cd8333e07beb5b1a4cdeb30fa8191b4d0
任天天 <ruattqwq@outlook.com>
2025-08-12 18:41:49 +0800

    feat(os-interop): 支持预写入 secret 并在运行时获取

--- Utils/OS/EnvironmentInterop.cs
+++ Utils/OS/EnvironmentInterop.cs
@@ -4,19 +4,41 @@ using PCL.Core.Utils.Exts;
 
 namespace PCL.Core.Utils.OS;
 
-public class EnvironmentInterop
+public static partial class EnvironmentInterop
 {
-    private const string ModuleEnvironment = "Environment";
+    private const string LogModule = "Environment";
 
+    /// <summary>
+    /// 读取环境变量并使用 <see cref="StringExtension.Convert{T}"/> 将其转换为指定类型并写入目标引用。
+    /// </summary>
+    /// <param name="key">环境变量名</param>
+    /// <param name="target">需要写入的目标引用 (不存在该环境变量或转换失败时不会写入)</param>
+    /// <param name="detailLog">是否在日志中输出变量值</param>
+    /// <typeparam name="TValue">目标引用的类型</typeparam>
+    /// <returns>是否成功写入目标引用</returns>
     public static bool ReadVariable<TValue>(string key, ref TValue target, bool detailLog = true)
     {
         var envValue = Environment.GetEnvironmentVariable(key);
         if (envValue == null) return false;
-        if (detailLog) LogWrapper.Trace(ModuleEnvironment, $"环境变量 {key} 值: {envValue}");
+        var valueLog = detailLog ? $" = {envValue}" : string.Empty;
+        LogWrapper.Debug(LogModule, $"读取到环境变量 {key}{valueLog}");
         var value = key.Convert<TValue>();
-        if (value == null) return false;
+        if (value == null)
+        {
+            LogWrapper.Warn(LogModule, $"环境变量 {key} 类型转换失败");
+            return false;
+        }
         target = value;
-        LogWrapper.Debug(ModuleEnvironment, $"成功读取环境变量 {key}");
         return true;
+    }
+
+    public static string? GetSecret(string key, bool readEnv = false, bool debugOnly = false)
+    {
+        SecretDictionary.TryGetValue(key, out var result);
+#if !DEBUG
+        if (debugOnly) return result;
+#endif
+        if (readEnv) ReadVariable($"PCL_{key}", ref result, false);
+        return result;
     }
 }
