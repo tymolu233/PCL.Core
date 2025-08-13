842a8802653be9e3f8e22bf4d8680b4de7e374b2
薄奚梦灵 <boximengling@outlook.com>
2025-08-13 22:08:35 +0800

    feat(yggdrasil): ALI 支持 (#36)
    * feat(yggdrasil): ALI 支持
    
    * sytle(yggdrasil): Remove useless nullable invoke
    
    * style(yggdrasil):remove useless using
    
    ---------
    
    Co-authored-by: 任天天 <61044187+ruattd@users.noreply.github.com>

---
+++ Minecraft/Yggdrasil/ApiLocation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using PCL.Core.Net;
+
+namespace PCL.Core.Minecraft.Yggdrasil;
+
+public static class ApiLocation
+{
+    public static async Task<string> GetApiLocation(string address)
+    {
+        if (!address.StartsWith("http")) address = $"https://{address}";
+        using var response = (await HttpRequestBuilder.Create(address, HttpMethod.Head).Build()).GetResponse();
+        response.Headers
+            .TryGetValues("X-Authlib-Injector-Api-Location", out var apiAddresses);
+        var currentApiAddr = new Uri(address);
+        if (apiAddresses is null) return string.Empty;
+        var apiAddr = apiAddresses.First() ?? "";
+        if (string.IsNullOrEmpty(apiAddr)) return address;
+        if (apiAddr.StartsWith(currentApiAddr.Scheme)) return apiAddr;
+        // 不允许 HTTPS 降 HTTP
+        if (apiAddr.StartsWith("http:") && currentApiAddr.Scheme == "https") return apiAddr.Replace("http","https");
+        return (new Uri(currentApiAddr, apiAddr)).ToString();   
+        
+    }
+}
