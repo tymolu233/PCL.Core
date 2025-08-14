004293f79aecab9cc9046e22e7f69a7d278fccfb
任天天 <ruattqwq@outlook.com>
2025-08-14 13:06:18 +0800

    imp(yggdrasil): 优化代码

--- Minecraft/Yggdrasil/ApiLocation.cs
+++ Minecraft/Yggdrasil/ApiLocation.cs
@@ -8,20 +8,17 @@ namespace PCL.Core.Minecraft.Yggdrasil;
 
 public static class ApiLocation
 {
-    public static async Task<string> GetApiLocation(string address)
+    public static async Task<string> TryRequest(string address)
     {
-        if (!address.StartsWith("http")) address = $"https://{address}";
-        using var response = (await HttpRequestBuilder.Create(address, HttpMethod.Head).Build()).GetResponse();
-        response.Headers
-            .TryGetValues("X-Authlib-Injector-Api-Location", out var apiAddresses);
-        var currentApiAddr = new Uri(address);
-        if (apiAddresses is null) return string.Empty;
-        var apiAddr = apiAddresses.First() ?? "";
-        if (string.IsNullOrEmpty(apiAddr)) return address;
-        if (apiAddr.StartsWith(currentApiAddr.Scheme)) return apiAddr;
+        var originAddr = address.StartsWith("http") ? address : $"https://{address}";
+        var originUri = new Uri(originAddr);
+        using var response = (await HttpRequestBuilder.Create(originAddr, HttpMethod.Head).Build()).GetResponse();
+        response.Headers.TryGetValues("X-Authlib-Injector-Api-Location", out var responses);
+        var resultAddr = responses?.First();
+        if (string.IsNullOrEmpty(resultAddr)) return originAddr;
+        if (resultAddr.StartsWith(originUri.Scheme)) return resultAddr;
         // 不允许 HTTPS 降 HTTP
-        if (apiAddr.StartsWith("http:") && currentApiAddr.Scheme == "https") return apiAddr.Replace("http","https");
-        return (new Uri(currentApiAddr, apiAddr)).ToString();   
-        
+        if (resultAddr.StartsWith("http:") && originUri.Scheme == "https") return resultAddr.Replace("http","https");
+        return (new Uri(originUri, resultAddr)).ToString();   
     }
 }
