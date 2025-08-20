c007c394b9f620fca016fa40fb67b4bce15a9ffb
任天天 <ruattqwq@outlook.com>
2025-08-20 18:53:26 +0800

    feat(task): 添加任务管理顶层接口

---
+++ App/Tasks/IPropertyChangedSource.cs
@@ -0,0 +1,17 @@
+﻿namespace PCL.Core.App.Tasks;
+
+public delegate void PropertyChangedHandler<in TProperty>(object source, TProperty oldValue, TProperty newValue);
+
+/// <summary>
+/// 可观察的属性值改变模型。
+/// </summary>
+/// <typeparam name="TProperty">属性类型。</typeparam>
+public interface IPropertyChangedSource<out TProperty>
+{
+    /// <summary>
+    /// 属性值改变事件。
+    /// 当你需要实现这个事件时，请保证只有在当前派生类型中可观察的一个或几个属性会触发这个事件，而不是像
+    /// <see cref="System.ComponentModel.INotifyPropertyChanged"/> 一样将所有属性的更改都传递过去。
+    /// </summary>
+    event PropertyChangedHandler<TProperty>? PropertyChanged;
+}
