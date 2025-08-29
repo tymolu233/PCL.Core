c658e0eaaf65cf73fa014dcbe9aeaebf2c3a8f53
EncVar <gbc034@outlook.com>
2025-08-29 16:16:29 +0800

    feat: task (#49)
    * feat(WIP): task
    
    * fix: pipeline progress not updated
    
    * style: redundant try-catch
    
    * pref: unified state event
    
    * fix: unsolved symbol
    
    * style: qodana
    
    * style: qodana
    
    * doc: fix
    
    * fix: test failure
    
    * fix: state not updated

---
+++ App/Tasks/PipelineTask.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PCL.Core.App.Tasks;
+public class PipelineTask<TLastResult> : TaskBase<TLastResult>
+{
+    public PipelineTask(string name, Delegate[] delegates, CancellationToken? cancellationToken = null, string? description = null) : base(name, cancellationToken, description)
+    {
+        List<TaskBase<object>> tasks = [];
+        var i = 0;
+        foreach (var task in delegates)
+        {
+            tasks.Add(new TaskBase<object>($"{name} - Pipe {i}", task)); 
+            i++; 
+        }
+        _tasks = tasks;
+        if (delegates.Last().Method.ReturnType != typeof(TLastResult))
+            throw new Exception($"[PipelineTask - {name}] 构造失败：不匹配的返回类型");
+        CancellationToken?.Register(() => { State = TaskState.Canceled; });
+    }
+
+    private readonly List<TaskBase<object>> _tasks;
+
+    public override TLastResult Run(params object[] objects)
+    {
+        State = TaskState.Running;
+        try
+        {
+            object lastResult = new();
+            foreach (var task in _tasks)
+                task.ProgressChanged += (_, o, n) =>
+                    Progress += (n - o) / _tasks.Count;
+            for (var i = 0; i < _tasks.Count; i++)
+            {
+                object[] param = [lastResult];
+                if (i == 0)
+                    param = objects;
+                CancellationToken?.ThrowIfCancellationRequested();
+                lastResult = _tasks[i].Run(param);
+            }
+            State = TaskState.Completed;
+            return Result = (TLastResult)lastResult;
+        }
+        catch (Exception)
+        {
+            if (!(CancellationToken?.IsCancellationRequested ?? false))
+                State = TaskState.Failed;
+            throw;
+        }
+    }
+
+    public override async Task<TLastResult> RunAsync(params object[] objects)
+    {
+        State = TaskState.Running;
+        try
+        {
+            foreach (var task in _tasks)
+                task.ProgressChanged += (_, o, n) =>
+                    Progress += (n - o) / _tasks.Count;
+            object lastResult = new();
+            for (var i = 0; i < _tasks.Count; i++)
+            {
+                object[] param = [lastResult];
+                CancellationToken?.ThrowIfCancellationRequested();
+                if (i == 0)
+                    param = objects;
+                lastResult = await _tasks[i].RunAsync(param);
+            }
+            State = TaskState.Completed;
+            return (TLastResult)lastResult;
+        }
+        catch (Exception)
+        {
+            if (!(CancellationToken?.IsCancellationRequested ?? false))
+                State = TaskState.Failed;
+            throw;
+        }
+    }
+
+    public override void RunBackground(params object[] objects)
+        => (BackgroundTask = RunAsync(objects)).Start();
+}
---
+++ App/Tasks/TaskBase.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PCL.Core.App.Tasks;
+
+public struct VoidResult;
+
+public class TaskBase<TResult> : IObservableTaskStateSource, IObservableProgressSource
+{
+    public TaskBase() 
+    { 
+        Name = ""; 
+        _delegate = () => { };
+    }
+    protected TaskBase(string name, CancellationToken? cancellationToken = null, string? description = null)
+    {
+        Name = name;
+        Description = description;
+        CancellationToken = cancellationToken;
+        _delegate = () => { };
+    }
+    public TaskBase(string name, Delegate loadDelegate, CancellationToken? cancellationToken = null, string? description = null)
+    {
+        Name = name;
+        _delegate = loadDelegate;
+        CancellationToken = cancellationToken;
+        Description = description;
+        CancellationToken?.Register(() => { State = TaskState.Canceled; });
+    }
+
+    event StateChangedHandler<double>? IStateChangedSource<double>.StateChanged
+    {
+        add => ProgressChanged += value;
+        remove => ProgressChanged -= value;
+    }
+    event StateChangedHandler<TaskState>? IStateChangedSource<TaskState>.StateChanged
+    {
+        add => StateChanged += value;
+        remove => StateChanged -= value;
+    }
+
+    public event StateChangedHandler<double>? ProgressChanged;
+    public event StateChangedHandler<TaskState>? StateChanged;
+
+    private double _progress = 0;
+    public double Progress {
+        get => _progress; 
+        set 
+        {
+            ProgressChanged?.Invoke(this, _progress, value);
+            _progress = value;
+        } 
+    }
+
+    public string Name { get; }
+    public string? Description { get; }
+
+    private TaskState _state = TaskState.Waiting;
+    public TaskState State
+    {
+        get => _state;
+        set
+        {
+            StateChanged?.Invoke(this, _state, value);
+            _state = value;
+        }
+    }
+
+    private TResult? _result;
+    public TResult? Result
+    {
+        get
+        {
+            if (BackgroundTask?.IsCompleted ?? false)
+                return BackgroundTask.Result;
+            return _result;
+        }
+        set => _result = value;
+    }
+
+    private readonly Delegate _delegate;
+    protected readonly CancellationToken? CancellationToken;
+
+    public virtual TResult Run(params object[] objects)
+    {
+        if (State != TaskState.Waiting)
+            throw new Exception($"[TaskBase - {Name}] 运行失败：任务已执行");
+        State = TaskState.Running;
+        try
+        {
+            var res = (TResult)(_delegate.DynamicInvoke([this, ..objects]) ?? new object());
+            CancellationToken?.ThrowIfCancellationRequested();
+            State = TaskState.Completed;
+            return _result = res;
+        }
+        catch (Exception)
+        {
+            if (!(CancellationToken?.IsCancellationRequested ?? false))
+                State = TaskState.Failed;
+            throw;
+        }
+    }
+
+    public virtual async Task<TResult> RunAsync(params object[] objects)
+    {
+        if (CancellationToken != null)
+            return _result = await Task.Run(() => Run(objects), cancellationToken: (CancellationToken)CancellationToken);
+        return _result = await Task.Run(() => Run(objects));
+    }
+
+    protected Task<TResult>? BackgroundTask;
+
+    public virtual void RunBackground(params object[] objects)
+        => (BackgroundTask = RunAsync(objects)).Start();
+}
