31f5d906533bfdda88b2102146debb1d867de555
Pigeon0v0 <60414767+Pigeon0v0@users.noreply.github.com>
2025-08-30 11:56:22 +0800

    imp(link): 给网络质量评估加了个描述 (#61)
    * imp(link): 给网络质量评估加了个描述
    
    * chore: 加个注释
    
    * refactor: 使用 Tuple
    
    * refactor: 换了 ValueTuple

--- Link/Lobby/LobbyTextHandler.cs
+++ Link/Lobby/LobbyTextHandler.cs
@@ -1,4 +1,5 @@
-﻿using PCL.Core.Link.EasyTier;
+﻿using System;
+using PCL.Core.Link.EasyTier;
 
 namespace PCL.Core.Link.Lobby;
 
@@ -23,10 +24,13 @@ public static class LobbyTextHandler
         _ => "未知"
     };
 
-    public static string GetQualityDesc(int quality) => quality switch
+    /// <summary>
+    /// 依据网络质量指数获取大厅连接状况文本
+    /// </summary>
+    public static (string Keyword, string Desc) GetQualityDesc(int quality) => quality switch
     {
-        >= 3 => "优秀",
-        >= 2 => "一般",
-        _ => "较差"
+        >= 3 => ("优秀", "当前网络环境不会影响联机体验\n该网络环境适合作为大厅创建者"),
+        >= 2 => ("一般", "当前网络环境可能会影响您的联机体验"),
+        _ => ("较差", "部分路由器和防火墙设置可能会影响您的联机体验")
     };
 }
