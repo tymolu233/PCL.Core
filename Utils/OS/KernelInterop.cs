29ca5cdfcb868444739a2e09ad4673941086a073
若琳酱 <208010141+ruollin@users.noreply.github.com>
2025-09-27 15:29:23 +0800

    feat(os): 使用 RtlGetNtVersionNumbers 获取真实版本号 (#82)
    * feat(os): 使用 RtlGetVersion 获取操作系统版本
    
    * docs(os): 添加注释
    
    * fix(os): 重复声明了 RtlGetVersion 函数
    
    * imp(os): 使用 RtlGetNtVersionNumbers 获取更精准的版本号

--- Utils/OS/KernelInterop.cs
+++ Utils/OS/KernelInterop.cs
@@ -30,6 +30,23 @@ public static partial class KernelInterop
 
     private const int ERROR_INSUFFICIENT_BUFFER = 122;
 
+    [LibraryImport("ntdll.dll")]
+    private static partial void RtlGetNtVersionNumbers(
+        out int major,
+        out int minor,
+        out int build);
+    /// <summary>
+    /// Retrieve the kernel version number of the current operating system (unaffected by compatibility settings)
+    /// </summary>
+    /// <returns>A <see cref="Version"/> instance, used to represent the current operating system kernel version number.</returns>
+    public static Version GetCurrentOSVersion()
+    {
+        RtlGetNtVersionNumbers(out var major, out var minor, out var build);
+        build &= 0xFFFF;
+        return new Version(major, minor, build);
+    }
+
+
     private enum LOGICAL_PROCESSOR_RELATIONSHIP : uint
     {
         RelationProcessorCore    = 0,
